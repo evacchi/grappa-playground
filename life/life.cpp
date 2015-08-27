ac132dc8a4ecea3b3f71c87b15be5e65a1eb79ab
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2015-08-27 12:10:38 +0000

    working life impl

--- life/life.cpp
+++ life/life.cpp
@@ -2,6 +2,8 @@
 #include <limits>
 #include <cassert>
 #include <array>
+#include <chrono>
+#include <thread>
 
 enum class State { Dead, Alive };
 
@@ -13,7 +15,7 @@ namespace Spaces {
         size_t width() { return upper-lower; }
         // Range() {}
         Range(long upper_) : upper(upper_) {
-            std::cout << "created: " << width() << "\n";
+            // std::cout << "range created: " << width() << "\n";
         }
         Range(long lower_, long upper_) : lower(lower_), upper(upper_) {}
         Range(const Spaces::Range& r): lower(r.lower), upper(r.upper) {}
@@ -28,7 +30,7 @@ namespace Spaces {
         Range x, y;
         Domain(const Domain& d) : x(d.x), y(d.y) {}
         Domain(Range x_, Range y_)  : x(x_), y(y_) {
-            std::cout << "created: " << x.width() << " x " << y.width() << "\n";
+            // std::cout << "domain created: " << x.width() << " x " << y.width() << "\n";
         }
         size_t size() {
             return x.width() * y.width();
@@ -50,40 +52,65 @@ namespace Spaces {
         Domain domain;
         State* values;
 
-        Board(Domain domain_) : domain(domain_) , values(new State[domain_.size()]) {}
+        Board(Domain domain_) : domain(domain_) , values(new State[domain_.size()]) {
+            for (int i = 0; i < domain_.size(); i++) values[i] = State::Dead;
+        }
         Board(Domain domain_, State* values_) : domain(domain_), values(values_) {}
+
+
+        long project(long i, long j) {
+            long result = domain.x.normalized(i) + domain.x.width() * domain.y.normalized(j);
+
+            // std::cout << i << ", " << j << " -> " << result << "\n";
+
+            return result;
+        }
 
         State& get(long i, long j) {
             // std::cout << i << "," << j << "\n";
             // assert ( i >= domain.x.lower && i < domain.x.upper 
             //         && j >= domain.y.lower && j < domain.y.upper );
-            return values[ domain.x.normalized(i) + domain.x.width() * domain.y.normalized(j) ];
+
+            // std::cout << i << ", " << j << " -> " 
+            //           << ((values[ project(i,j) ] == State::Alive)? "alive":"dead") << "\n";
+
+            return values[ project(i,j) ];
         }
 
         State& operator()(long i, long j) {
             return get(i,j);
         }
 
-        Board& slice(Range xx, Range yy) {
-            Domain new_domain(xx, yy);
-            Board new_board (new_domain);    
+        // Board& slice(Range xx, Range yy) {
+        //     Domain new_domain(xx, yy);
+        //     Board new_board (new_domain);    
 
+        //     // quick&dirty: this should become a memcpy!                        
+        //     for (int i = new_board.domain.x.lower; i < domain.x.upper; i++) {
+        //         for (int j = new_board.domain.y.lower; j < domain.y.upper; j++) {
+        //             new_board(i,j) = get(i,j);
+        //         }
+        //     }
+
+        //     return new_board;
+        // }
+
+        // Board& operator()(Range xx, Range yy) {
+        //     return slice(xx,yy);
+        // }
+
+        // Board& operator()(Domain xy) {
+        //     return slice(xy.x,xy.y);
+        // }
+
+        void assign(Board value) {
             // quick&dirty: this should become a memcpy!                        
-            for (int i = new_board.domain.x.lower; i < domain.x.upper; i++) {
-                for (int j = new_board.domain.y.lower; j < domain.y.upper; j++) {
-                    new_board(i,j) = get(i,j);
+            for (int i = value.domain.x.lower; i < value.domain.x.upper; i++) {
+                for (int j = value.domain.y.lower; j < value.domain.y.upper; j++) {
+                    get(i,j) = value.get(i,j);
                 }
             }
 
-            return new_board;
-        }
-
-        Board& operator()(Range xx, Range yy) {
-            return slice(xx,yy);
-        }
-
-        Board& operator()(Domain xy) {
-            return slice(xy.x,xy.y);
         }
 
 
@@ -109,29 +136,20 @@ public:
         board(grid_domain) { }
         
 
-    /*
-      proc step(){
-     
-        var tempGrid: [this.computeDomain] State;
-        forall (i,j) in this.computeDomain {
-          var isAlive = this.grid[i,j] == State.alive;
-          var numAlive = (+ reduce this.grid[ i-1..i+1, j-1..j+1 ]) - if isAlive then 1 else 0;      
-          tempGrid[i,j] = if ( (2 == numAlive && isAlive) || numAlive == 3 ) then State.alive else State.dead ;
-        }
-     
-        this.grid[this.computeDomain] = tempGrid;
-     
-      }
-    */
-
     State& operator()(const size_t& x,  const size_t& y) {
         return board(x,y);
     }
 
-    size_t count_alive(Spaces::Board b) {
+    size_t count_alive(Spaces::Range x, Spaces::Range y) {
         size_t total = 0;
-        for (int i = 0; i < b.domain.size(); i++) {
-            if(b.values[i] == State::Alive) total++;  
+        for (int i = x.lower; i <= x.upper; i++) {
+            for (int j = y.lower; j <= y.upper; j++) {
+                if(board.get(i,j) == State::Alive) {
+                    // std::cout << i << ", " << j << " is alive\n";
+                    total++;  
+                }
+
+            }
         }
         return total;
     }
@@ -144,30 +162,38 @@ public:
         for (long i = 0; i < grid_domain.x.upper; i++) {
             for (long j = 0; j < grid_domain.y.upper; j++) {
                 bool is_alive = board.get(i,j) == State::Alive;
-                size_t num_alive = count_alive( board.slice( Spaces::Range(i-1,i+1), Spaces::Range(j-1, j+1) ) ) - (is_alive? 1 : 0);
+                size_t num_alive = count_alive( Spaces::Range(i-1,i+1), Spaces::Range(j-1, j+1) );
+                if (is_alive) num_alive--;
+                // std::cout << i << ", " << j <<  ": num_alive = " << num_alive 
+                //           << " => " << (( (2 == num_alive && is_alive) || num_alive == 3 ) ? "alive" : "dead") << "\n"; 
                 temp_board(i,j) = ( (2 == num_alive && is_alive) || num_alive == 3 ) ? State::Alive : State::Dead ;
             }
         }
 
-        board(compute_domain) = temp_board;
+        board.assign(temp_board);
 
     } 
 
-    void pretty_print(Spaces::Domain grid_domain, Spaces::Board board) {
+    void pretty_print() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << (char)27 << "[2J" << (char)(27) << "[;H";
         for (long i = 0; i < grid_domain.x.upper; i++) {
             for (long j = 0; j < grid_domain.y.upper; j++) {
-                std::cout << ( board.get(i,j) == State::Alive? "#" : " " );
+                if (i == 0 || i == grid_domain.x.upper - 1) std::cout << "-";
+                else if (j == 0 || j == grid_domain.y.upper - 1) std::cout << "|";
+                else std::cout << ( board.get(i,j) == State::Alive? "#" : " " );
             }
             std::cout << "\n";
         }
+
     }
  
 
 
 };
 
-const int grid_height = 3;
-const int grid_width = 3;
+const int grid_height = 10;
+const int grid_width = 10;
 
 
 void main_body() {
@@ -175,8 +201,14 @@ void main_body() {
     game(grid_height/2 + 1, grid_width/2    ) = State::Alive;
     game(grid_height/2 + 1, grid_width/2 + 1) = State::Alive;
     game(grid_height/2 + 1, grid_width/2 + 2) = State::Alive;
+        
+    game.pretty_print();
 
-    game.step();
+    for (int i = 0; i < 3; i++) {
+        game.step();
+        game.pretty_print();
+    }
+
 }
 
 int main(int argc, char* argv[])
